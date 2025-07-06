83cf1a588495a5cbe5c2a11290d91bf8e031f664
ashish.purseth <ashishpurseth88@gmail.com>
2025-07-06 11:11:28 +0530

    add stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,8 @@
 /** @type {import('stylelint').Config} */
 export default {
-    extends: [
-        'stylelint-config-standard-scss', // SCSS support + standard rules
-        'stylelint-config-prettier-scss' // disables conflicting prettier rules
-    ],
+    extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
     plugins: ['stylelint-scss'],
     rules: {
-        // fixes issue with unknown SCSS at-rules like @use, @include, etc.
         'at-rule-no-unknown': null,
         'scss/at-rule-no-unknown': true,
         'selector-class-pattern': null
